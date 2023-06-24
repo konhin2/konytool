2efa38d55e1eec6a0cdb310fd554de876819c630
Konhin Claro <konhinclaro@gmail.com>
2023-06-23 23:58:23 -0600

    initial commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+import type { V2_MetaFunction } from "@remix-run/node"
+
+export const meta: V2_MetaFunction = () => {
+	return [{ title: "Home" }]
+}
+
+export default function Index() {
+	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+}
